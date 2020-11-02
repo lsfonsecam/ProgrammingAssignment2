4c191f215842fec407cc09c7af9ed798f2d96275
lsfonsecam <62046738+lsfonsecam@users.noreply.github.com>
2020-11-02 04:07:38 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function caches the inverse of a given "matrix". 
+## The second function returns the inverse of a "matrix"
 
-## Write a short comment describing this function
+#___
+## The function caches the inverse of a given matrix, and returns that matrix and its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+  inv <<- NULL 
+  matrix <- function(y){
+    m <<- y
+  }
+  inv <<- solve(m)
+  inv
+  list(matrix = m, inverse = inv)
 }
 
+#   m <- matrix(rnorm(9), nrow = 3)            #  This is an example of a matrix that was used
+#   makeCacheMatrix(m)
 
-## Write a short comment describing this function
+#___ 
+## This second function returns the inverse of a given matrix. If this matrix was used with the "makeCacheMatrix" function 
+## then the "cacheSolve" function will return the inverse previously stored in the cache, otherwise, it will return the 
+## inverse of the matrix that is entered in this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(w = matrix()){
+  if (w[1] == makeCacheMatrix(m)$matrix[1] ) {
+    inv <- makeCacheMatrix(m)$inverse
+    if (!is.null(inv)) {
+      return(inv)
+    }
+  } else {
+    return(solve(w))
+  }
 }
+
+#     a <- matrix(rnorm(9),nrow = 3)           #  "a" is a different matrix than the "m" matrix used in the first function
+
+#     cacheSolve(a)                            #  returns the inverse of "a" (can be compared to the one that returns "solve (a)")
+#     cacheSolve(m)                            #  returns the inverse of "m" that was previously stored with the first function
